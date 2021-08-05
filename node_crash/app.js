efaf317918dbabd7d1db3594706b21c1ebd2f4b8
Faiyaz Rafeek <faiyazah_ct16014@stu.kln.ac.lk>
2021-08-05 13:48:25 +0530

    finished ejs tempalte engine

--- node_crash/app.js
+++ node_crash/app.js
@@ -1,20 +1,29 @@
 const express = require('express');
 
 // express app
-
 const app = express();
+
+// register vie engine
+app.set('view engine', 'ejs');
 
 // listen for request
 app.listen(5000);
 
 app.get('/', (req, res) => {
-    // res.send('<p>home page</p>');
-    res.sendFile('./views/index.html', {root: __dirname });
+    const blogs = [
+        {title: 'Lorem ipsum dolor, sit amet consectetur adipisicing', snippet: 'Lorem, ipsum dolor sitonsectetur adipisicing elit. Vero, dolorem! Similique nobis molestias vel officia corrupti sunt quidem nesciunt repellendus?'},
+        {title: 'ipsum dolor, sit amet consectetur adipisicing', snippet: 'Loret consectetur adipisicing elit. Vero, dolorem! Similique nobis molestias vel officia corrupti sunt quidem nesciunt repellendus?'},
+        {title: 'sit amet consectetur adipisicing', snippet: 'Lorem, ipsum dolor sit amet ipisicing elit. Vero, dolorem! Similique nobis molestias vel officia corrupti sunt quidem nesciunt repellendus?'}
+    ]
+    res.render('index', {title: 'Home', blogs});
 });
 
 app.get('/about', (req, res) => {
-    // res.send('<p>home page</p>');
-    res.sendFile('./views/about.html', {root: __dirname });
+    res.render('about', {title: 'About'});
+});
+
+app.get('/blogs/create', (req, res) => {
+    res.render('create', {title: 'Create'});
 });
 
 // redirects
@@ -24,5 +33,5 @@ app.get('/about-us', (req, res) =>{
 
 // 404 page
 app.use((req, res) => {
-    res.status(404).sendFile('./views/404.html', {root: __dirname });
+    res.status(404).render('404', {title: '404'})
 })
